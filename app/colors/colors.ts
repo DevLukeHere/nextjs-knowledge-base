e81a197c07e6057ccdf20f5bb50fd2e594409d19
Luke Wong <lukewong93@gmail.com>
2023-06-16 00:14:00 +0800

    style: update colour name

--- app/colors/colors.ts
+++ app/colors/colors.ts
@@ -2,7 +2,7 @@ const Colors = {
   white: "#ffffff",
   black: "#373737",
   primary: "#03a84e",
-  grey: "#9c9aa6",
+  primaryGrey: "#9c9aa6",
   secondaryGrey: "#fafafa",
   tertiaryGrey: "#eeeeee",
 };
