75961c4c5c0fc8ddaed51350ba3c31f6fa0c0b52
Luke Wong <lukewong93@gmail.com>
2023-06-19 22:41:43 +0800

    style: create breadcrumbs component

---
+++ app/components/Breadcrumbs.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Colors from "../colors/colors";
+
+export default function Breadcrumbs() {
+  return <Box></Box>;
+}
