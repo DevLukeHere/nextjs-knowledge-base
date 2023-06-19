b8604c1951f3d8d6686ff6f0b3d7268c4e62d0a0
Luke Wong <lukewong93@gmail.com>
2023-06-19 18:19:42 +0800

    style: add homepage title & subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,38 @@
+"use client";
+
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Colors from "./colors/colors";
+
 export default function Home() {
   return (
     <main>
-      <h1>This is the homepage!</h1>
+      <Box sx={{ paddingTop: "3.5rem", paddingBottom: "2.5rem" }}>
+        <Typography
+          sx={{
+            fontWeight: "700",
+            color: Colors.black,
+            textAlign: "center",
+            lineHeight: "2.5rem",
+            marginBottom: "0.5rem",
+          }}
+          variant="h4"
+        >
+          Knowledge base does not have to be boring
+        </Typography>
+        <Typography
+          sx={{
+            fontWeight: "400",
+            color: Colors.black,
+            textAlign: "center",
+            lineHeight: "1.5rem",
+          }}
+          variant="h6"
+        >
+          Everything you need to manage your messaging
+        </Typography>
+      </Box>
     </main>
   );
 }
