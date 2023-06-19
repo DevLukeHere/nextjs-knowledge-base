3d0b72ab56d016a0757ebfa00ea653a3e2bcc7bc
Luke Wong <lukewong93@gmail.com>
2023-06-19 18:50:30 +0800

    build: configure fontawesome

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
 import { ReactNode } from "react";
 import { Lato } from "next/font/google";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+config.autoAddCss = false
 
 const lato = Lato({
   weight: ["100", "300", "400", "700", "900"],
