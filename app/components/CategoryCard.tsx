7986a7674d94a3badf2bc81130b43afc880d5c2c
Luke Wong <lukewong93@gmail.com>
2023-06-20 23:46:30 +0800

    refactor: migrate inline styles to stylesheet

--- app/components/CategoryCard.tsx
+++ app/components/CategoryCard.tsx
@@ -1,26 +1,26 @@
 "use client";
 
-import { useAppDispatch, useAppSelector } from "../redux/hooks";
 import { Fragment } from "react";
 import {
   BoxStyled,
   CategoryCardSection,
   SkeletonStyled,
+  LinkStyled,
+  UpdatedAtText,
+  TitleText,
+  SubtitleText,
 } from "./styles/CategoryCard.styles";
 import "dayjs/plugin/relativeTime";
 import useCategories from "../hooks/useCategories";
 import dayjs from "dayjs";
-import Colors from "../colors/colors";
-import Typography from "@mui/material/Typography";
 import Image from "next/image";
 
 dayjs.extend(require("dayjs/plugin/relativeTime"));
 
 export default function CategoryCard() {
-  // const dispatch = useAppDispatch();
   const { categories, isLoading } = useCategories();
 
-  console.log("categories:", categories);
+  // console.log("categories:", categories);
 
   return (
     <CategoryCardSection>
@@ -38,30 +38,21 @@ export default function CategoryCard() {
           ?.filter((category: any) => category.enabled === true)
           .sort((a: any, b: any) => a.order - b.order)
           .map((category: any) => (
-            <BoxStyled key={category?.id}>
-              <Image
-                src={`/images/${category?.icon}.svg`}
-                width={44}
-                height={55}
-                alt={`${category.icon} icon`}
-              />
-              <Typography
-                sx={{
-                  textTransform: "capitalize",
-                  color: Colors.black,
-                  marginBottom: "1.25rem",
-                }}
-                variant="h6"
-              >
-                {category?.title}
-              </Typography>
-              <Typography sx={{ color: Colors.primary }} variant="body2">
-                {category?.totalArticle} articles
-              </Typography>
-              <Typography sx={{ color: Colors.primaryGrey }} variant="caption">
-                Last updated {dayjs(category?.updatedOn).fromNow()}
-              </Typography>
-            </BoxStyled>
+            <LinkStyled key={category?.id} href={`/category/${category?.id}`}>
+              <BoxStyled>
+                <Image
+                  src={`/images/${category?.icon}.svg`}
+                  width={50}
+                  height={55}
+                  alt={`${category.icon} icon`}
+                />
+                <TitleText>{category?.title}</TitleText>
+                <SubtitleText>{category?.totalArticle} articles</SubtitleText>
+                <UpdatedAtText>
+                  Last updated {dayjs(category?.updatedOn).fromNow()}
+                </UpdatedAtText>
+              </BoxStyled>
+            </LinkStyled>
           ))
       )}
     </CategoryCardSection>
--- app/components/styles/CategoryCard.styles.ts
+++ app/components/styles/CategoryCard.styles.ts
@@ -1,8 +1,33 @@
 import styled from "@mui/material/styles/styled";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import Link from "next/link";
 import Skeleton from "@mui/material/Skeleton";
 import Colors from "../../colors/colors";
+
+export const TitleText = styled("h3")({
+  textTransform: "capitalize",
+  color: Colors.black,
+  marginBottom: "1.25rem",
+  fontSize: "1.25rem",
+});
+
+export const SubtitleText = styled("p")({
+  color: Colors.primary,
+  fontSize: "0.875rem",
+  margin: "0",
+});
+
+export const UpdatedAtText = styled("p")({
+  color: Colors.primaryGrey,
+  fontSize: "0.75rem",
+  margin: "0",
+});
+
+export const LinkStyled = styled(Link)({
+  textDecoration: "none",
+  width: "fit-content",
+});
 
 export const SkeletonStyled = styled(Skeleton)({
   width: "19.75rem",
@@ -30,6 +55,7 @@ export const CategoryCardSection = styled(Box)((props) => ({
   gridTemplateColumns: "auto auto auto",
   gridTemplateRows: "auto auto",
   rowGap: "1.25rem",
+  justifyItems: "center",
 
   [props.theme.breakpoints.down("lg")]: {
     gridTemplateColumns: "auto auto",
